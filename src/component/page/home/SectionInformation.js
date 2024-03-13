87b77378f41779726132833e0c46e9f4f2577d15
RioBoy <boyrio68@gmail.com>
2024-03-13 20:15:16 +0800

    fixing error => building and deployment

--- src/component/page/home/SectionInformation.js
+++ src/component/page/home/SectionInformation.js
@@ -157,9 +157,9 @@ const SectionInformation = ({ data = {} }) => {
 
         <p className="mobile-fs-20 fs-20 fw-500 mb-0">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
+          industry. Lorem Ipsum has been the industry&lsquo;s standard dummy
+          text ever since the 1500s, when an unknown printer took a galley of
+          type and scrambled it to make a type specimen book.
         </p>
       </div>
     </section>
